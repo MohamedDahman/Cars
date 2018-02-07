4bb1f4790dd7f610f1c2fc81ab78e065dccad916
Mohamed Dahman <mhd.dahman@yahoo.com>
2018-02-07 12:56:16 +0000

    Database + Application.py + hellper. to start work

---
+++ helpers.py
@@ -0,0 +1,45 @@
+import csv
+import urllib.request
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///cars.db")
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def apology(message, code=400):
+    """Renders message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
